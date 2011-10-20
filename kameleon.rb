1acfb3c42d88525be2a376d96fd355d33e30c12e
Philippe Le Brouster <philippe.le-brouster@imag.fr>
2011-10-20 16:10:43 +0200

    Imported Upstream version 1.2.3+snapshot.161.c582391

--- kameleon.rb
+++ kameleon.rb
@@ -263,7 +263,7 @@ end
 $cur_dir=Dir.pwd
 $var_dir="/var/lib/kameleon"
 $kameleon_dir=File.dirname($0)
-version="1.2.2"
+version="1.2.3"
 required_globals = ["distrib", "workdir_base"]
 required_commands = ["chroot", "which", "cat", "echo"]
 
