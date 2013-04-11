da77e8cf1e42fe6048154fa661b58a0deea405f5
Cristian RUIZ <cristian.ruiz@imag.fr>
2013-04-11 14:35:24 +0200

    updating the git repository

---
+++ scripts/ruby/run_list_generator.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby 
+
+recipies=""
+
+ARGV.each do|a|
+ recipies+="\"recipe[#{a}]\""
+ recipies+=" , " unless a == ARGV.last
+end
+
+File.open("/tmp/node.json","w") do |file|
+
+  file.write("{\n")
+  file.write("\"run_list\": [ #{recipies} ] \n")
+  file.write("}\n")
+
+end
