ddc1cec9c8fa89a35a9daaf5814cd41489073ff3
Cristian Ruiz <cristian.ruiz@imag.fr>
2013-06-05 13:29:29 +0200

    Updating Kameleon with New recipes: Ubuntu quantal, SLC (scientific Linux) New steps: blueprint, ccontorl (Linux module installation), Grid5000 deployable image. New script: VirtualBox_deploy , simple script to deploy the image generated into a VirtualBox virtual machine

---
+++ scripts/VirtualBox_deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+NAME=$1
+VBOX_DISK=$2
+
+VBoxManage createvm --name $NAME --register
+VBoxManage modifyvm $NAME --memory 512
+VBoxManage storagectl $NAME --name SATA --add sata --controller IntelAhci --bootable on --sataportcount 1
+VBoxManage storageattach $NAME --storagectl SATA --port 0 --device 0 --type hdd --medium $VBOX_DISK
+#VBoxManage modifyvm $NAME --nic1 hostonly
+VBoxManage startvm $NAME
