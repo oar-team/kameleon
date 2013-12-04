6b35c6f32d23adc2ee9c697363020b57a579daa0
Salem Harrache <salem.harrache@inria.fr>
2013-12-04 11:03:48 +0100

    write a dummy recipe and test it

---
+++ tests/test_recipe.rb
@@ -0,0 +1,13 @@
+require File.expand_path("../helper", __FILE__)
+require 'test/unit'
+require 'session'
+
+class Kameleon_tests < Test::Unit::TestCase
+
+  def test_dummy_recipe
+    sh = Session::new
+    sh.execute 'sudo bundle exec kameleon recipes/dummy_recipe.yaml'
+    assert_equal(0, sh.exit_status)
+  end
+
+end
