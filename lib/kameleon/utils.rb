f08cbce6950edf13d9346016f997fa5405723bc7
Salem Harrache <salem.harrache@inria.fr>
2013-12-12 00:06:14 +0100

    remove color functions helpers

--- lib/kameleon/utils.rb
+++ lib/kameleon/utils.rb
@@ -30,14 +30,4 @@ module Kameleon
       super(key)
     end
   end
-
-  ### helper functions for output colorizing
-  def colorize(text, color_code)
-    "#{color_code}#{text}\e[0m"
-  end
-
-  def red(text); colorize(text, "\e[31m\e[1m"); end
-  def green(text); colorize(text, "\e[32m\e[1m"); end
-  def blue(text); colorize(text, "\e[34m\e[1m"); end
-  def cyan(text); colorize(text, "\e[36m"); end
 end
