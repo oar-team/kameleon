409e77c3aea274cd4537015954ebf2618543a928
Salem Harrache <salem.harrache@inria.fr>
2013-12-14 16:43:42 +0100

    added pp_s function which allows prety print object into string

--- lib/kameleon/utils.rb
+++ lib/kameleon/utils.rb
@@ -1,33 +1,48 @@
+require 'pp'
+require 'stringio'
+
 module Kameleon
-  ### Hash that keeps elements in the insertion order -- it's more
-  ### convenient for storing macrostep->microstep->comand structure
-  class OrderedHash < Hash
-    def initialize
-      @key_list = []
-      super
+  module Utils
+
+    def self.pp_s(*objs)
+        s = StringIO.new
+        objs.each {|obj|
+          PP.pp(obj, s)
+        }
+        s.rewind
+        s.read
     end
-    def []=(key, value)
-      if has_key?(key)
-        super(key, value)
-      else
-        @key_list.push(key)
-        super(key, value)
+
+    ### Hash that keeps elements in the insertion order -- it's more
+    ### convenient for storing macrostep->microstep->comand structure
+    class OrderedHash < Hash
+      def initialize
+        @key_list = []
+        super
       end
-    end
-
-    def by_index(index)
-      self[@key_list[index]]
-    end
-
-    def each
-      @key_list.each do |key|
-        yield( [key, self[key]] )
+      def []=(key, value)
+        if has_key?(key)
+          super(key, value)
+        else
+          @key_list.push(key)
+          super(key, value)
+        end
       end
-    end
 
-    def delete(key)
-      @key_list = @key_list.delete_if { |x| x == key }
-      super(key)
+      def by_index(index)
+        self[@key_list[index]]
+      end
+
+      def each
+        @key_list.each do |key|
+          yield( [key, self[key]] )
+        end
+      end
+
+      def delete(key)
+        @key_list = @key_list.delete_if { |x| x == key }
+        super(key)
+      end
     end
   end
 end
