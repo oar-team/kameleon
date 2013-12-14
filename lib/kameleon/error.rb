2feff1cd25c76250ac681e59d6d9074e9f6502ba
Salem Harrache <salem.harrache@inria.fr>
2013-12-14 08:49:29 +0100

    run kameleon with friendly errors

--- lib/kameleon/error.rb
+++ lib/kameleon/error.rb
@@ -1,3 +1,5 @@
+require 'thor/error'
+
 module Kameleon
   class KameleonError < ::StandardError
     def self.status_code(code)
@@ -10,4 +12,31 @@ module Kameleon
   class ArgumentError < KameleonError; status_code(4) ; end
   class ContextError < KameleonError; status_code(5) ; end
   class SyntaxError < KameleonError; status_code(6) ; end
+
+
+  def self.with_friendly_errors
+    yield
+  rescue Kameleon::KameleonError => e
+    Kameleon.ui.error e.message, :wrap => true
+    Kameleon.ui.trace e
+    exit e.status_code
+  rescue Thor::UndefinedTaskError => e
+    Kameleon.ui.error e.message
+    exit 15
+  rescue Thor::Error => e
+    Kameleon.ui.error e.message
+    exit 15
+  rescue Interrupt => e
+    Kameleon.ui.error "\nQuitting..."
+    Kameleon.ui.trace e
+    exit 1
+  rescue SystemExit => e
+    exit e.status
+  rescue Exception => e
+    Kameleon.ui.debug "Unexpected error occurred :\n#{e}"
+    Kameleon.ui.error <<-ERR, :wrap => true
+Unfortunately, a fatal error has occurred. Use --debug option for more details
+ERR
+    raise e
+  end
 end
