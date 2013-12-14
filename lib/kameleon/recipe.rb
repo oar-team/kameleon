579411c88897de78414e9463d3aae4c7ce1b01ca
Salem Harrache <salem.harrache@inria.fr>
2013-12-14 16:54:35 +0100

    refactor Recipe and simplify load! method

--- lib/kameleon/recipe.rb
+++ lib/kameleon/recipe.rb
@@ -7,7 +7,7 @@ module Kameleon
     attr_accessor :sections
 
     # define section constant
-    class Section < OrderedHash
+    class Section < Utils::OrderedHash
       BOOTSTRAP="bootstrap"
       SETUP="setup"
       EXPORT="export"
@@ -19,15 +19,16 @@ module Kameleon
         ]
       end
     end
+
     attr_accessor :global, :sections, :check_cmds
-    def initialize(env, path)
-      @env = env
-      @name = File.basename( path, ".yaml" )
-      @path = path
+
+    def initialize(path)
+      @path = Pathname.new(path)
+      @name = (@path.basename ".yaml").to_s
       @check_cmds = []
       @sections = Section.new
       @global = { "distrib" => nil,
-                  "workdir" => File.join(@env.build_dir, @name),
+                  "workdir" => File.join(Kameleon.env.build_dir, @name),
                   "rootfs" => "$$workdir/chroot",
                   "exec_cmd" => "fakechroot $$rootfs" }
       load!
@@ -35,44 +36,27 @@ module Kameleon
 
     def load!
       # Find recipe path
-      @env.logger.info('recipe') { 'Loading ' + @path }
+      Kameleon.ui.info "Loading #{@path}"
       yaml_recipe = YAML.load File.open @path
-
-      fail InternalError, "Invalid yaml error" unless yaml_recipe.kind_of? Hash
-      fail InternalError, "Recipe misses 'global' section" unless yaml_recipe.key? "global"
+      fail RecipeError, "Invalid yaml error" unless yaml_recipe.kind_of? Hash
+      fail RecipeError, "Recipe misses 'global' section" unless yaml_recipe.key? "global"
 
       #Load Global variables
       @global.merge!(yaml_recipe.fetch("global"))
-
       @global.each do |key, value|
-        fail "Recipe misses required variable: #{key}" if value.nil?
+        fail RecipeError, "Recipe misses required variable: #{key}" if value.nil?
       end
 
       #Find and load steps
-      Section.sections.each do |section|
-        @sections[section]= []
-        yaml_recipe.fetch(section).each do |macrostep|
-
-          #check if it's a string or a dict
-          if macrostep.kind_of? String
-            step = macrostep
-          elsif macrostep.kind_of? Hash
-            step = macrostep.keys[0]
-            #Load options
-            options = macrostep.values[0]
-          else
-            fail "Malformed yaml recipe in section: "+ section
-          end
-
-          # find the path of the macrostep
-          step_path = find_macrostep(step, section)
-
+      Section.sections.each do |section_name|
+        @sections[section_name]= []
+        yaml_recipe.fetch(section_name).each do |macrostep_yaml|
+          macrostep_instance = load_macrostep(macrostep_yaml, section_name)
           # save the macrostep in the section
-          @sections[section].push(Macrostep.new(step_path, options))
+          @sections[section_name].push(macrostep_instance)
         end
       end
     end
-
 
     def load_macrostep(raw_macrostep, section_name)
       #check if it's a string or a dict
