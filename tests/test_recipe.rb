d7404a4ae8b45199c5a7101bc8d568b9ba9bbd87
Salem Harrache <salem.harrache@inria.fr>
2013-12-17 15:42:18 +0100

    Started unit testing and get a pretty coverage page

--- tests/test_recipe.rb
+++ tests/test_recipe.rb
@@ -2,12 +2,12 @@ require File.expand_path("../helper", __FILE__)
 require 'test/unit'
 require 'session'
 
-class Kameleon_tests < Test::Unit::TestCase
+# class Kameleon_tests < Test::Unit::TestCase
 
-  def test_dummy_recipe
-    sh = Session::new
-    sh.execute 'sudo bundle exec kameleon recipes/dummy_recipe.yaml'
-    assert_equal(0, sh.exit_status)
-  end
+#   def test_dummy_recipe
+#     sh = Session::new
+#     sh.execute 'sudo bundle exec kameleon recipes/dummy_recipe.yaml'
+#     assert_equal(0, sh.exit_status)
+#   end
 
-end
+# end
