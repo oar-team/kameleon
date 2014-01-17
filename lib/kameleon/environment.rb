5d1ee6223aa22ad06e597d4864a29e9af88941b1
Salem Harrache <salem.harrache@inria.fr>
2014-01-17 16:16:13 +0100

    Expanded relative path

--- lib/kameleon/environment.rb
+++ lib/kameleon/environment.rb
@@ -15,10 +15,10 @@ module Kameleon
       @logger = Log4r::Logger.new("kameleon::[env]")
       # symbolify commandline options
       options = options.inject({}) {|result,(key,value)| result.update({key.to_sym => value})}
-      workspace = options[:workspace]
-      build_path = options[:build_path] || File.join(workspace, "builds")
+      workspace = File.expand_path(options[:workspace])
+      build_path = File.expand_path(options[:build_path]) || File.join(workspace, "builds")
       defaults = {
-        :workspace => Pathname.new(File.join(Kameleon.source_root, 'templates')),
+        :workspace => Pathname.new(workspace),
         :templates_path => Pathname.new(File.join(Kameleon.source_root, 'templates')),
         :recipes_path => Pathname.new(File.join(workspace, "recipes")),
         :build_path => Pathname.new(build_path),
