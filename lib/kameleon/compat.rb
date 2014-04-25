92dea6d6bc19092d8aed995786dc3ea31c7e2079
Salem Harrache <salem.harrache@inria.fr>
2014-04-25 14:04:36 +0200

    Fixed YAML warning message when using ruby >= 2.0.0 (#1)

--- lib/kameleon/compat.rb
+++ lib/kameleon/compat.rb
@@ -1,33 +1,7 @@
 require 'securerandom'
 require 'yaml'
-require 'syck'
 
-# Fast and backwards compatible YAML with Syck
-# Snippet from http://developer.zendesk.com/blog/2013/10/03/using-syck-for-yaml/
-if RUBY_VERSION >= "1.9.3"
-  # to force yaml to dump ASCII-8Bit strings as strings
-  YAML::ENGINE.yamler = 'syck'
-  engine = YAML::ENGINE
-
-  def engine.yamler=(value)
-    return if value == 'syck'
-    raise ArgumentError, "Already using Syck, cannot change to #{value}"
-  end
-
-  # Disable Psych in Rubygems
-  ENV['TEST_SYCK'] = 'true'
-end
-
-# force UTF-8 on all strings back from YAML.
-class << YAML::DefaultResolver
-  alias_method :node_import_without_utf8, :node_import
-
-  def node_import(node)
-    val = node_import_without_utf8(node)
-    val.force_encoding("UTF-8") if val.respond_to?(:force_encoding)
-    val
-  end
-end
+YAML::ENGINE.yamler = 'syck' if (defined?(Syck) || defined?(YAML::Syck)) && defined?(YAML::ENGINE)
 
 if RUBY_VERSION < "1.9.3"
   # Backport of missing SecureRandom methods from 1.9
