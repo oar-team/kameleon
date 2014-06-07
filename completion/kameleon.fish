4ac36dd05ceab47ef4df5fe1f402c121e5520f48
Salem Harrache <salem.harrache@inria.fr>
2014-06-07 09:35:13 +0200

    fix fish autocompletion

--- completion/kameleon.fish
+++ completion/kameleon.fish
@@ -2,8 +2,9 @@ complete -c kameleon -l help --description "Describe available commands or one s
          -a 'help new templates version build checkpoints clear'
 
 complete -c kameleon -l build --description "Builds the appliance from the recipe"
-complete -c kameleon -l clear --description "Cleaning out context and removing all checkpoints"
-complete -c kameleon -l checkpoints --description "Describe available commands or one specific command"
+complete -c kameleon -l clean --description "Cleaning 'out' and 'local' context and removing all checkpoints"
+complete -c kameleon -l checkpoints --description "Lists all availables checkpoints"
 complete -c kameleon -l version --description "Prints the Kameleon's version information"
 complete -c kameleon -l new --description "Creates a new recipe"
+complete -c kameleon -l import --description "Imports the given template"
 complete -c kameleon -l templates --description "Lists all defined templates"
