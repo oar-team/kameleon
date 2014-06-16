065a7b9c3530f30df23bc3dba6f64fed8c750984
Salem Harrache <salem.harrache@inria.fr>
2014-06-16 18:58:36 +0200

    Bump version
    Changes : minor bug fix about variables resolution

--- lib/kameleon/utils.rb
+++ lib/kameleon/utils.rb
@@ -2,7 +2,7 @@ module Kameleon
   module Utils
 
     def self.resolve_vars(raw, yaml_path, initial_variables, kwargs = {})
-      strict = kwargs.fetch('strict', true)
+      strict = kwargs.fetch(:strict, true)
       raw.to_s.gsub(/\$\$\{[a-zA-Z0-9\-_]+\}|\$\$[a-zA-Z0-9\-_]+/) do |var|
         # remove the dollars
         if var.include? "{"
@@ -14,7 +14,7 @@ module Kameleon
         if initial_variables.has_key? strip_var
           value = initial_variables[strip_var]
         else
-          if strict
+          if !strict
             fail RecipeError, "#{yaml_path}: variable #{var} not found in local or global" \
           end
         end
